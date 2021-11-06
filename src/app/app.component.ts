5a88cfa4bc07001910d3a7aa017c2e2ae43c91ba
MohamedKasem55 <81971922+MohamedKasem55@users.noreply.github.com>
2021-11-06 19:38:19 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'todoTask';
+  newTodo:string;
+  todos:Array<string>
+  constructor(){
+    this.newTodo='';
+    this.todos=[]
+  }
+  ngOnInit(){
+    this.fetchTodosLocalStorage()
+  }
+  
+
+  addTodo(){
+    this.todos.push(this.newTodo);
+    localStorage.setItem('todos',JSON.stringify(this.todos))
+  }
+  removeTodo(i){
+    this.todos.splice(i,1)
+    localStorage.setItem('todos',JSON.stringify(this.todos))
+  }
+  fetchTodosLocalStorage(){
+    let storedTodos=localStorage.getItem('todos')
+    if(!!storedTodos)
+    this.todos=[...JSON.parse(storedTodos)]
+  }
 }
